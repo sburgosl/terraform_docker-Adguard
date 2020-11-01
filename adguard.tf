e882d8572b5ef86b1ee42b218f3b7e520fd60bab
sburgosl <>
2020-11-01 12:36:27 +0000

    feat: create adguard container. Include traefik option

---
+++ adguard.tf
@@ -0,0 +1,125 @@
+# Docker AdGuard Home
+
+resource "docker_image" "adguardhome" {
+  name         = "adguard/adguardhome:${var.adguard_version}"
+  keep_locally = true
+}
+
+resource "docker_container" "adguardhome" {
+  image     = docker_image.adguardhome.latest
+  name      = "adguard"
+  hostname  = "rpi_adguard"
+  restart   = "unless-stopped"
+
+  # HTTP
+  dynamic "ports" {
+    for_each = var.use_traefik ? [] : [1]
+    content {
+      internal = var.int_http_ui_port
+      external = var.ext_http_ui_port
+    }
+  }
+  # HTTPS
+  dynamic "ports" {
+    for_each = var.use_https && !var.use_traefik ? [1] : []
+    content {
+      internal = 443
+      external = 443 
+    }
+  }
+  
+  # DNS
+  ports {
+    internal = 53
+    external = 53
+    protocol = "tcp"
+  }
+  ports {
+    internal = 53
+    external = 53
+    protocol = "udp"  
+  }
+  dynamic "ports" {
+    for_each = var.use_https ? [1] : []
+    content {
+      internal = 853
+      external = 853
+    }
+  }
+
+  # DHCP
+  dynamic "ports" {
+    for_each = var.use_dhcp ? [1] : []
+    content {
+      internal = 67
+      external = 67
+      protocol = "udp"
+    }
+  }
+  dynamic "ports" {
+    for_each = var.use_dhcp ? [1] : []
+    content {
+      internal = 68
+      external = 68
+      protocol = "tcp"
+    }
+  }
+  dynamic "ports" {
+    for_each = var.use_dhcp ? [1] : []
+    content {
+      internal = 68
+      external = 68
+      protocol = "udp"
+    }
+  }
+
+  # First time config
+  dynamic "ports" {
+    for_each = var.fist_config ? [1] : []
+    content {
+      internal = 3000
+      external = 3000
+    }
+  }
+
+  # Bind mounts
+  volumes { 
+    host_path      = var.adguard_workdir
+    container_path = "/opt/adguardhome/work"
+  }
+  volumes {
+    host_path      = var.adguard_confdir
+    container_path = "/opt/adguardhome/conf"
+  }
+
+  # Traefik
+  dynamic "labels" {
+    for_each = var.use_traefik ? [1] : []
+    content {
+      label = "traefik.enable"
+      value = "true"
+    }
+  }
+  dynamic "labels" {
+    for_each = var.use_traefik ? [1] : []
+    content {
+      label = "traefik.http.routers.adguard.rule"
+      value = "Host(`adguard.homenet`)"
+    }
+  }
+  dynamic "labels" {
+    for_each = var.use_traefik ? [1] : []
+    content {
+      label = "traefik.http.routers.adguard.entrypoints"
+      value = var.use_https ? "websecure" : "web"
+    }
+  }
+  dynamic "labels" {
+    for_each = var.use_traefik ? [1] : []
+    content {
+      label = "traefik.http.services.adguard.loadbalancer.server.port"
+      value = var.use_https ? "443" : var.int_http_ui_port
+    }
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+# Docker AdGuard Variables file
+
+variable "adguard_version" {
+  type        = string
+  default     = "latest"
+  description = "Docker Image Tag"
+}
+
+variable "fist_config" {
+  type        = bool
+  default     = false
+  description = "Expose tcp/3000 for first config"
+}
+
+variable "use_https" {
+  type        = bool
+  default     = false
+  description = "Enable HTTPS port for Web UI"
+}
+
+variable "use_dhcp" {
+  type        = bool
+  default     = false
+  description = "Enable Adguard DHCP server"
+}
+
+variable "use_traefik" {
+  type        = bool
+  default     = false
+  description = "Enable Adguard DHCP server"
+}
+
+variable "ext_http_ui_port" {
+  type        = number
+  default     = 80
+  description = "Host TCP Port Web UI"
+}
+
+variable "int_http_ui_port" {
+  type        = number
+  default     = 80
+  description = "Container TCP Port Web UI. Adguard Config"
+}
+
+variable "adguard_workdir" {
+  type        = string
+  default     = "/opt/adguard/workdir"
+  description = "AdGuard WorkDir Folder"
+}
+
+variable "adguard_confdir" {
+  type        = string
+  default     = "/opt/adguard/confdir"
+  description = "AdGuard ConfDir Folder"
+}
